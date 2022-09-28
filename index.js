6424ea48fc8f5b2dc0a921a4e299dac60f0a3418
KanizFatimaMitu <kanizfatima528@gmail.com>
2022-09-28 23:00:04 +0600

    mongo connection build

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-require ('dotenv').config();
 const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.POrt || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// kaniz
+// FQ9VftzJYPjKBXnM
+
+const uri = "mongodb+srv://kaniz:FQ9VftzJYPjKBXnM@cluster0.8n7ml4a.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello from learning with kaniz &sohel from yt')
+    res.send('hello')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
